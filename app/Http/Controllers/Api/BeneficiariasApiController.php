7527d66cea749979862ec758e454c5beff367d23
danielmestre98 <dmestreloureiro@gmail.com>
2024-06-17 19:03:00 +0000

    Estrutura api (sem código ainda)

---
+++ app/Http/Controllers/Api/BeneficiariasApiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BeneficiariasApiController extends Controller
+{
+    function retrieveStatus(Request $request)
+    {
+    }
+}
